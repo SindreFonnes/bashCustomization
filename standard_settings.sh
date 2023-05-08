def08722941cc85b61c69326e71b8f501bdea3d2
Sindre Fonnes <sindre.fonnes@scaleaq.com>
2023-05-08 07:42:27 +0200

    Added prompt customization

--- standard_settings.sh
+++ standard_settings.sh
@@ -1,6 +1,9 @@
 # Standard settings
 PROMPT_DIRTRIM=3;
 
+# https://phoenixnap.com/kb/change-bash-prompt-linux // some ways to customize it
+PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\n\e[0;32m> \e[0m"
+
 if [[ $SHELL == "/usr/bin/zsh" ]]; then
 	#if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin ]; then
 	#	git clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin;
