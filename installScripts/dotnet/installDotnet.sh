e6ddd87731281cb6e09573eab4cd2ed5f733b5d8
Sindre Fonnes <sindre.fonnes@gmail.com>
2024-09-06 17:06:57 +0200

    Updated dotnet install script

--- installScripts/dotnet/installDotnet.sh
+++ installScripts/dotnet/installDotnet.sh
@@ -40,8 +40,6 @@ install_for_ubuntu () {
     sudo apt-get install apt-transport-https -y && \
     sudo apt-get update && \
   	sudo apt-get install -y dotnet-sdk-8.0 && \
-	sudo apt-get update && \
-  	sudo apt-get install -y aspnetcore-runtime-8.0
 
     script_success_message "$name";
     exit 0;
