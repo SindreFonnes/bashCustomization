fce8c6c1923a602d4636bee52b9abe2c77a7c5ec
Sindre Fonnes <sindre.fonnes@protonmail.com>
2026-06-07 09:06:22 +0200

    Added nvim update alias

--- programExtensions/nvim/nvimAliases.sh
+++ programExtensions/nvim/nvimAliases.sh
@@ -1,1 +1,2 @@
 alias vi="nvim";
+alias update-nvim='curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage && chmod u+x nvim-linux-x86_64.appimage && sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim';
